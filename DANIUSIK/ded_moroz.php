79abd1b7db05e04c5c5a88db9534b51ac6202a0d
Dupeljan Linux <dupeljan@gmail.com>
2019-03-31 08:28:18 +0300

    refactoring

--- DANIUSIK/ded_moroz.php
+++ DANIUSIK/ded_moroz.php
@@ -1,43 +1,20 @@
 <html>
 <body>
 
+	<h1> 
+	 </h1>
 <form action="<?php echo $_SERVER['PHP_SELF'];?>" method="post">
 	<?php
+		include 'include/utilities.php';
+		$conn = new db();
+		
+		$sql = "SELECT caption FROM wishes";
+		$result = $conn->get_query($sql);
+		for ($x = 0; $x <= 10; $x++) {	
+			create_select("wishes" . $x,$result,"caption");
+			echo "<br>";
+		}
 
-	  function connect_to_db(){
-	  	$servername = "localhost";
-		$username = "root";
-		$password = "242555";
-		$dbname = "mydb";
-
-
-		// Create connection
-		$conn = new mysqli($servername, $username, $password, $dbname);
-		//mysql_query("SET NAMES UTF8");
-		$conn->set_charset("utf8");
-		//header('Content-Type: text/html; charset=utf-8')
-		// Check connection
-		if ($conn->connect_error) {
-		    die("Connection failed: " . $conn->connect_error);
-		} 
-		return $conn;
-	  }
-
-	  $conn = connect_to_db();
-	  for ($x = 0; $x <= 10; $x++) {	
-			  echo "<select name=\"wishes\" >";
-			  $sql = "SELECT caption FROM wishes";
-			  $result = $conn->query($sql);
-
-			if ($result->num_rows > 0) {
-		    	// output data of each row
-		    	while($row = $result->fetch_assoc()) {
-		    		echo "<option value=\"" . $row['caption'] ."\">" . $row['caption'] . "</option>";
-		    	}
-			}
-
-			echo "</select><br>";
-	  }
 	 ?>
 	
 	<br>
@@ -48,7 +25,7 @@
 	<input type="submit">
 </form>
 
-
+<h4> I receve </h4>
 
 </body>
 </html>
---
+++ DANIUSIK/include/utilities.php
@@ -0,0 +1,46 @@
+<?php
+	/**
+	 * database handler class
+	 */
+	class db 
+	{	
+		private $conn;
+		function __construct(){
+		  	$servername = "localhost";
+			$username = "root";
+			$password = "242555";
+			$dbname = "mydb";
+
+
+			// Create connection
+			$this->$conn = new mysqli($servername, $username, $password, $dbname);
+			//mysql_query("SET NAMES UTF8");
+			$this->$conn->set_charset("utf8");
+			// Check connection
+			if ($conn->connect_error) {
+			    die("Connection failed: " . $conn->connect_error);
+			} 
+
+			
+		}
+
+		function get_query($sql){
+			// return mysqli_result object
+			return $this->$conn->query($sql);
+		}
+	}
+
+	function create_select($name,$mysqli_result,$key){
+		echo "<select name=\"" . $name ."\" >";
+		if ($mysqli_result->num_rows > 0) {
+		   	// output data of each row
+		   	while($row = $mysqli_result->fetch_assoc()) {
+		   		echo "<option value=\"" . $row[$key] ."\">" . $row[$key] . "</option>";
+		   	}
+		}
+		echo "</select>";
+		$mysqli_result->data_seek(0);
+	}
+	
+
+?>
