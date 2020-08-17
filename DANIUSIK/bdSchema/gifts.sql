960c61e90ed7d713cbccdef326f9f75932dc155d
Dupeljan Linux <dupeljan@gmail.com>
2020-08-17 08:00:53 +0300

    Add bdSchema and report

---
+++ DANIUSIK/bdSchema/gifts.sql
@@ -0,0 +1,22 @@
+/*
+-- Query: 
+-- Date: 2019-03-30 17:12
+*/
+INSERT INTO `gifts` (`name`,`cost`,`sizes_idsizes`) VALUES ('Сборник анекдотов Петросяна','0',1);
+INSERT INTO `gifts` (`name`,`cost`,`sizes_idsizes`) VALUES ('Машинка','10',1);
+INSERT INTO `gifts` (`name`,`cost`,`sizes_idsizes`) VALUES ('Большая машинка','20',1);
+INSERT INTO `gifts` (`name`,`cost`,`sizes_idsizes`) VALUES ('Визит КГБ','-20',1);
+INSERT INTO `gifts` (`name`,`cost`,`sizes_idsizes`) VALUES ('Визит налоговой','-20',1);
+INSERT INTO `gifts` (`name`,`cost`,`sizes_idsizes`) VALUES ('Рособрнадзор','-20',1);
+INSERT INTO `gifts` (`name`,`cost`,`sizes_idsizes`) VALUES ('Паравозик','15',1);
+INSERT INTO `gifts` (`name`,`cost`,`sizes_idsizes`) VALUES ('Барагозик','20',1);
+INSERT INTO `gifts` (`name`,`cost`,`sizes_idsizes`) VALUES ('Лишай','-10',1);
+INSERT INTO `gifts` (`name`,`cost`,`sizes_idsizes`) VALUES ('Артрит','-20',1);
+INSERT INTO `gifts` (`name`,`cost`,`sizes_idsizes`) VALUES ('Остеохондрос','-20',1);
+INSERT INTO `gifts` (`name`,`cost`,`sizes_idsizes`) VALUES ('Носки','5',1);
+INSERT INTO `gifts` (`name`,`cost`,`sizes_idsizes`) VALUES ('Носки с авокадо','6',1);
+INSERT INTO `gifts` (`name`,`cost`,`sizes_idsizes`) VALUES ('Акула из икеи','40',1);
+INSERT INTO `gifts` (`name`,`cost`,`sizes_idsizes`) VALUES ('Путевка в дет. дом','-30',1);
+INSERT INTO `gifts` (`name`,`cost`,`sizes_idsizes`) VALUES ('АААВТОМОБИИИЛЬ','20',1);
+INSERT INTO `gifts` (`name`,`cost`,`sizes_idsizes`) VALUES ('Ведро мороженного с ванилью','10',1);
+INSERT INTO `gifts` (`name`,`cost`,`sizes_idsizes`) VALUES ('Сопли',1);
---
+++ DANIUSIK/bdSchema/wishes.sql
@@ -0,0 +1,49 @@
+/*
+-- Query: 
+-- Date: 2019-03-30 16:51
+*/
+INSERT INTO `wishes` (`caption`,`score`) VALUES ('Здоровья',3);
+INSERT INTO `wishes` (`caption`,`score`) VALUES ('Счастья',4);
+INSERT INTO `wishes` (`caption`,`score`) VALUES ('Любви',5);
+INSERT INTO `wishes` (`caption`,`score`) VALUES ('Ни гвоздя ни жезла',-1);
+INSERT INTO `wishes` (`caption`,`score`) VALUES ('Не хворай',1);
+INSERT INTO `wishes` (`caption`,`score`) VALUES ('Удачи',6);
+INSERT INTO `wishes` (`caption`,`score`) VALUES ('Красивой бороды',10);
+INSERT INTO `wishes` (`caption`,`score`) VALUES ('Чтоб в пробках не стоял',8);
+INSERT INTO `wishes` (`caption`,`score`) VALUES ('Меньше оленей(на дороге)',7);
+INSERT INTO `wishes` (`caption`,`score`) VALUES ('Больше оленей(в санях)',5);
+INSERT INTO `wishes` (`caption`,`score`) VALUES ('Чтобы ты дожил до слеющего нового года',1);
+INSERT INTO `wishes` (`caption`,`score`) VALUES ('Чтобы снегурочка не растяла',8);
+INSERT INTO `wishes` (`caption`,`score`) VALUES ('Чтоб снегурка не растолстела',5);
+INSERT INTO `wishes` (`caption`,`score`) VALUES ('Вкусных cookie',8);
+INSERT INTO `wishes` (`caption`,`score`) VALUES ('Теплых носков',2);
+INSERT INTO `wishes` (`caption`,`score`) VALUES ('Кэшбэка с алихи',10);
+INSERT INTO `wishes` (`caption`,`score`) VALUES ('Упасть с саней в грязь',-5);
+INSERT INTO `wishes` (`caption`,`score`) VALUES ('Замернуть в сугробе',-6);
+INSERT INTO `wishes` (`caption`,`score`) VALUES ('Прожорливых оленей',-1);
+INSERT INTO `wishes` (`caption`,`score`) VALUES ('Чтоб сцыганили твоих оленей',-5);
+INSERT INTO `wishes` (`caption`,`score`) VALUES ('Чтоб тебя Санта Клаус побил',-20);
+INSERT INTO `wishes` (`caption`,`score`) VALUES ('Денег',20);
+INSERT INTO `wishes` (`caption`,`score`) VALUES ('Закрыть ипотеку',20);
+INSERT INTO `wishes` (`caption`,`score`) VALUES ('Чтоб налоговая не трогала',10);
+INSERT INTO `wishes` (`caption`,`score`) VALUES ('Много плохих детей',0);
+INSERT INTO `wishes` (`caption`,`score`) VALUES ('Маленькой пенсии',-3);
+INSERT INTO `wishes` (`caption`,`score`) VALUES ('Артрита',-10);
+INSERT INTO `wishes` (`caption`,`score`) VALUES ('Не сгореть в камине',8);
+INSERT INTO `wishes` (`caption`,`score`) VALUES ('Хороших мальчиков и девочек',9);
+INSERT INTO `wishes` (`caption`,`score`) VALUES ('Зачета по веб программированию',6);
+INSERT INTO `wishes` (`caption`,`score`) VALUES ('Канализации',8);
+INSERT INTO `wishes` (`caption`,`score`) VALUES ('Комуналку поменьше',4);
+INSERT INTO `wishes` (`caption`,`score`) VALUES ('Чтоб елка сломалась',-6);
+INSERT INTO `wishes` (`caption`,`score`) VALUES ('Диареи',-7);
+INSERT INTO `wishes` (`caption`,`score`) VALUES ('Чтоб эльфы образовали профсоюз',-9);
+INSERT INTO `wishes` (`caption`,`score`) VALUES ('Правой палочки твикс',8);
+INSERT INTO `wishes` (`caption`,`score`) VALUES ('Левой палочки твикс',-8);
+INSERT INTO `wishes` (`caption`,`score`) VALUES ('Не всретить M&mсов',10);
+INSERT INTO `wishes` (`caption`,`score`) VALUES ('Снегурки - феминистки',-8);
+INSERT INTO `wishes` (`caption`,`score`) VALUES ('Чтоб у оленей хвост замерз',-5);
+INSERT INTO `wishes` (`caption`,`score`) VALUES ('Чтоб у оленей нос замерз',-7);
+INSERT INTO `wishes` (`caption`,`score`) VALUES ('Чтоб норм было',-10);
+INSERT INTO `wishes` (`caption`,`score`) VALUES ('Сладкого рулета',4);
+INSERT INTO `wishes` (`caption`,`score`) VALUES ('Чтоб тебя в деддом сдали',-8);
+INSERT INTO `wishes` (`caption`,`score`) VALUES ('Чтоб ты растаял',-10);
